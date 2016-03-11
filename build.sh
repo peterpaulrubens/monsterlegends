e7eb068543a7149e75d207d9ad65867fb8762918
peterpaulrubens <ozzy_ball@yahoo.com>
2016-03-11 11:22:46 -0800

    Build one JSON file per tier, in preparation for different pages or tabs per tier.

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ cd data
 cat common.csv uncommon.csv rare.csv epic.csv legendary.csv > all_monsters.csv
 
 # the next step requires the Perl module App::fsql to be installed
-cat header.json > monsters.json
-fsql  -aoh -a all_monsters.csv -f json "SELECT * FROM all_monsters" >> monsters.json
-cat footer.json >> monsters.json
+for tier in common uncommon rare epic legendary all_monsters; do
+    cat header.json > ${tier}.json;
+    fsql  -aoh -a ${tier}.csv -f json "SELECT * FROM ${tier}" >> ${tier}.json;
+    cat footer.json >> ${tier}.json;
+done;
